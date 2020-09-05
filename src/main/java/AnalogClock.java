db2acd2d5530f1c2a5045de61ba74b52b0ae803d
Jomar Jocson <jomar.224585@nv.ccsd.net>
2020-09-05 23:55:28 +0000

    Analog completed

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -9,12 +9,18 @@ class AnalogClock {
         //Reads ints from user
         int angles = scanner.nextInt();
         int anglesPerHour = 30;
+        int anglePerMin = 12;
         int angleRemainder;
+        int angleMultipliedMin;
+
         /*
          *  your code goes here
          */
+
         angleRemainder = angles % anglesPerHour;
-        System.out.print(angleRemainder);
+        angleMultipliedMin = angleRemainder * anglePerMin;
+
+        System.out.print(angleMultipliedMin);
 
         // closing the scanner object
         scanner.close();

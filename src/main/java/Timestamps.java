041619ccc8128fd4414ba1b6ac74f87d1ec2f233
Jomar Jocson <jomar.224585@nv.ccsd.net>
2020-09-05 22:46:42 +0000

    Finished timestamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -13,13 +13,25 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
+        int deltaHours;
+        int deltaMin;
+        int deltaSec;
+        int hours;
+        int min;
+        int sec;
+
 
 
         /*
          *  your code goes here
          */
-
-
+        deltaHour = hours2-hours1;
+        deltaHour = minutes2-minutes1;
+        deltaHour = seconds2-seconds1;
+        hours = deltaHours * 3600;
+        min = deltaMin * 60;
+        sec = deltaSec * 1;
+        System.out.print(hours + " " + min + " " + sec);
         // closing the scanner object
         scanner.close();
     }

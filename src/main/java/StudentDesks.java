d051a37473cca12196bfd90a95f524e003ab2f65
Jomar Jocson <jomar.224585@nv.ccsd.net>
2020-09-06 00:12:29 +0000

    Studentdesks checkpoint

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,26 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int classA = scanner.nextInt();
+        int classB = scanner.nextInt();
+        int classC = scanner.nextInt();
+        int divideA;
+        int remainder
+        int divideB;
+        int divideC;
+        int addAll;
 
         /*
          *  your code goes here
          */
+          divideA = classA / 2;
 
+          divideB = classB / 2;
+
+          divideC = classC / 2;
+
+          addAll = divideA + divideB + divideC;
+          System.out.print(addAll);
 
         // closing the scanner object
         scanner.close();
